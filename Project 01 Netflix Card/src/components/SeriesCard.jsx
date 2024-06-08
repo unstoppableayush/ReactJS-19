9c4b40c66bce4ccfb7290be74aab5569df4d5cfe
unstoppableayush <ayushkum884@gmail.com>
2024-06-08 22:57:28 +0530

    Day 14 Destucturing in React

--- Project 01 Netflix Card/src/components/SeriesCard.jsx
+++ Project 01 Netflix Card/src/components/SeriesCard.jsx
@@ -1,13 +1,14 @@
 export const SeriesCard = (props) => {
     console.log(props)
+    const {name, rating, description} = props.series
   return (
     <li>
       <div>
         <img src="quejpgen." alt="" height="300px" width="500px" />
       </div>
-      <h2>Name: {props.series.name}</h2>
-      <h3>Rating: {props.series.rating}</h3>
-      <p>Description: {props.series.description}</p>
+      <h2>Name: {name}</h2>
+      <h3>Rating: {rating}</h3>
+      <p>Description: {description}</p>
       <p>Cast: {props.series.cast.join(", ")}</p>
       <p>Genre: {props.series.genre.join(", ")}</p>
       <a href={props.series.watch_url} target="_blank">
